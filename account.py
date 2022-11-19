5467289e6b040c7d5bf19cdb48a55ff6d2ccc3c2
Olay02 <118495129+Olay02@users.noreply.github.com>
2022-11-18 19:58:32 -0600

    added second file

--- account.py
+++ account.py
@@ -1,27 +1,49 @@
-
 class Account:
-    def __init__(self, name):
-        self.account_name = name
-        self.account_balance = 0
+    def __init__(self, name: str) -> None:
+        """
+        inputs the user's name and sets the account balance to 0
+        :param name:str -> None
+        """
 
-    def deposit(self, amount):
+        self.__account_name = name
+        self.__account_balance = 0
+
+    def deposit(self, amount: float) -> bool:
+        """
+        Checks to see if the amount inputted is greater than 0
+
+        :param amount: -> float
+        :return: -> bool
+        """
         if amount <= 0:
             return False
         else:
-            self.account_balance += amount
+            self.__account_balance += amount
             return True
 
-    def withdraw(self, amount):
-        if amount <= 0 or amount > self.account_balance:
+    def withdraw(self, amount: float) -> bool:
+        """
+        Checks to see if the amount inputted is greater than 0 and the current balance
+
+        :param amount: -> float
+        :return: -> bool
+        """
+        if amount <= 0 or amount > self.__account_balance:
             return False
         else:
-            self.account_balance -= amount
+            self.__account_balance -= amount
             return True
 
-    def get_balance(self):
-        return self.account_balance
+    def get_balance(self) -> float:
+        """
+        returns the balance of the account
+        :return: -> float
+        """
+        return self.__account_balance
 
-    def get_name(self):
-        return self.account_name
-
-
+    def get_name(self) -> str:
+        """
+        returns the name on the account
+        :return: -> str
+        """
+        return self.__account_name
